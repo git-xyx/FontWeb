f133959b30d80dda6cf5419cce72a44082f15635
xyx <18559488952@163.com>
2021-02-25 21:57:47 +0800

    update 添加node-sass
    增加sass-loader

--- vue/hello-world/src/components/HelloWorld.vue
+++ vue/hello-world/src/components/HelloWorld.vue
@@ -2,6 +2,12 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
   </div>
+  <div class="wrapper">
+    <div style="background: red"></div>
+    <div style="background: green"></div>
+    <div style="background: blue"></div>
+    <div style="background: black"></div>
+  </div>
 </template>
 
 <script>
@@ -14,7 +20,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 h3 {
   margin: 40px 0 0;
 }
@@ -28,5 +34,15 @@ li {
 }
 a {
   color: #42b983;
+}
+.wrapper {
+  display: flex;
+  width: 1000px;
+  border: 1px solid;
+
+  div {
+    flex-basis: 100px;
+    height: 200px;
+  }
 }
 </style>
