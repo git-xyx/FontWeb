8d3020387a044fc600d89b5827e89d19785aa825
xyx <18559488952@163.com>
2022-03-26 01:14:34 +0800

    requestAnimationFrame

--- vue/trip_vue2/src/pages/css-module/list-pages/see_1.vue
+++ vue/trip_vue2/src/pages/css-module/list-pages/see_1.vue
@@ -1,20 +1,55 @@
 <template>
   <div class="content">
     <h2>see1</h2>
-    <div></div>
+    <div class="moon"></div>
+
+    <!-- requestAnimationFrame  -->
+    <div style="position: relative">
+      <div id="e"></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  mounted() {},
+  mounted() {
+    var e = document.getElementById("e");
+    var flag = true;
+    var left = 0;
+
+    function selfRender() {
+      if (flag == true) {
+        if (left >= 100) {
+          flag = false;
+        }
+        e.style.left = ` ${left++}px`;
+      } else {
+        if (left <= 0) {
+          flag = true;
+        }
+        e.style.left = ` ${left--}px`;
+      }
+    }
+    // setInterval(function () {
+    //   selfRender();
+    // }, 1000 / 60);
+    (function animloop(time) {
+      console.log(time, performance.now());
+      selfRender();
+      let rafId = requestAnimationFrame(animloop);
+      //如果left等于50 停止动画
+      if (left == 50) {
+        cancelAnimationFrame(rafId);
+      }
+    })();
+  },
 };
 </script>
 
 <style lang="less" scoped>
 .content {
   background-color: #000;
-  div {
+  .moon {
     --colorA: #fff;
     position: relative;
     width: 300px;
@@ -25,6 +60,16 @@ export default {
     filter: drop-shadow(0 0 2px var(--colorA))
       drop-shadow(0 0 5px var(--colorA)) drop-shadow(0 0 10px var(--colorA))
       drop-shadow(0 0 20px var(--colorA));
+  }
+
+  #e {
+    width: 100px;
+    height: 100px;
+    background: red;
+    position: absolute;
+    left: 0;
+    top: 0;
+    zoom: 1;
   }
 }
 </style>
